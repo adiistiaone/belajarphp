24db36386f0ec771afb8d6296c28600d4300745f
adiistiaone <adisetiawantm18@gmail.com>
2022-08-01 21:02:13 +0700

    menambahkan file pertemuan 3 dan file latihan 1 - 3

---
+++ pertemuan3/latihan1.php
@@ -0,0 +1,24 @@
+<?php
+// Pengulangan
+// For, While, Do... While, Foreach (pengulangan khusus array)
+
+
+// for ($i = 0; $i < 5; $i++) {
+//     echo "Hello world <br>";
+// }
+
+// While pengulangan yang akan terus berjalan selama nilainya "true"
+$i = 0;
+while ($i < 5) {
+    echo "Hellow World <br>";
+$i++
+}
+
+// Do While = Lakukan hal inii, selama kondisi bernilai "true"
+$i = 0;
+do {
+    echo "Hello World <br>";
+$i++
+} while ( $i < 5 );
+
+?>
---
+++ pertemuan3/latihan3.php
@@ -0,0 +1,17 @@
+<?php
+// Pengkondisian / percabangan
+// if else
+// if else if else
+// ternary
+// switch
+
+$x = 10;
+if ( $x < 20; ) {
+    echo "Benar";
+} else if( $x = 20; ) {
+    echo "Bingo!";
+} else {
+    echo "salah";
+}
+
+?>
