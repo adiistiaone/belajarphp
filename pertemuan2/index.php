55430f91b339bf5d7d025afee06153621d54ae6f
adiistiaone <adisetiawantm18@gmail.com>
2022-08-01 17:59:23 +0700

    menambahkan folder pertemuan2 dan file index.php

---
+++ pertemuan2/index.php
@@ -0,0 +1,31 @@
+<?php
+//ini adalah komentar
+/*
+ini komentar juga
+*/
+
+
+//  Pertemuan 2 - PHP dasar
+/*  Standar output
+- echo, print
+- print_r
+- var_dump
+*/
+
+$nama = "Adi Setiawan";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Dasar</title>
+</head>
+<body>
+    
+    <h1>Hallo Selamat Datang <?= $nama ?></h1>
+
+</body>
+</html>
